bfcb57912e1e446c79956514ffb8039ea12184a3
Andreea <bociu.andreea@gmail.com>
2021-01-04 17:09:05 +0100

    Created main app, created some of the main components and configured routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { SettingsComponent } from './settings/settings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TopNavComponent,
+    SideNavComponent,
+    MyAccountComponent,
+    SettingsComponent,
+    DashboardComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
